e88616f8d5ee789feaf9c37b85b725a81e906e8f
Israel Yance <israelyance@gmail.com>
2020-07-06 21:48:21 -0500

    04-creacion-primera-app

---
+++ posts/views.py
@@ -0,0 +1,40 @@
+'''Posts Views.'''
+
+# Django
+from django.http import HttpResponse
+
+# Utilities
+from datetime import datetime
+
+
+posts = [
+    {
+        'name': 'Mont Blac',
+        'user': 'Israel Yance',
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'picture': 'https://picsum.photos/200/200/?image=1036',
+    },
+    {
+        'name': 'Via Láctea',
+        'user': 'Samuel Yance',
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'picture': 'https://picsum.photos/200/200/?image=903',
+    },
+    {
+        'name': 'Nuevo Auditorio',
+        'user': 'Raquel Yance',
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'picture': 'https://picsum.photos/200/200/?image=1076',
+    }
+]
+
+def list_posts(request):
+    '''List existing posts.'''
+    content = []
+    for post in posts:
+        content.append("""
+            <p><strong>{name}</strong></p>
+            <p><small>{user} - <i>{timestamp}</i></small></p>
+            <figure><img src="{picture}"></figure>
+        """.format(**post))
+    return HttpResponse('<br>'.join(content))
